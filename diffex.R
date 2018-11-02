01f7af66c603a63f36a1c2f92b6e4932aac4d630
djs134 <djs134@pitt.edu>
2018-11-02 11:45:39 -0400

    Add files via upload

--- diffex.R
+++ diffex.R
@@ -1,7 +1,7 @@
 #if(!require(devtools)) install.packages("devtools")
 #devtools::install_github("kassambara/ggpubr")
 
-data <- read.csv('GSE7390_2.csv', header = FALSE, sep=',') ##loads the data
+data <- read.csv('1456_2.csv', header = FALSE, sep=',') ##loads the data
 
 #separates into positive and negative groups
 pos <- data[which(data[,22284]==1),]
@@ -18,7 +18,6 @@ i = c(1:22283)
 #orders the features indices high to low, according the difference of means
 m = order(val,1:22283,decreasing=TRUE)
 
-
 n = dim(data)[1]
 s = floor(n/5)
 x = data[,1:22284] #matrix of predictors and classes
@@ -26,38 +25,64 @@ y = data[,22284] #vector of labels
 rand = sample(1:n,n) #creates a random permutation (for indexing samples randomly)
 x = x[rand,]
 
-best_i=1 #assume using 5 features has the best accuracy
-overall_acc = 0 #assume the accuracy of the best value of i is 0
-#performs 5-fold cross validation using i features with greatest difference in mean
-for (i in 5:7){ #set to 7 for testing script; change to desired number when actually running
-	acc = c(0,0,0,0,0)
-	for (j in 0:4){
-		xtr = x[-c(1:s+j*s),] #excludes (j+1)th fold for cross validation
-								#do note that we may leave out some samples entirely (up to 4)
-		xtr = data.frame(xtr[,c(22284, m[1:i])]) #glm() wants a dataframe; glm() predicts the first column on the remaining columns
-		xte = x[c(1:s+j*s),] #select the values from the left out fold
-		xte = data.frame(xte[,m[1:i]]) #create a dataframe of the left out fold, for testing
-		yte = x[c(1:s+j*s),22284] #vector of actual classes for the leftout fold, used for comparison to predicted classes
-		model = glm(xtr, family=binomial(link="logit")) #i'm not sure if these are appropriate parameters
-		p= predict(model, xte, type = "response")
-		print(length(which(p>=.5)))
-		#1
-		#2
+
+
+
+
+	best_k=1 #assume using 5 features has the best accuracy
+	best_acc = 0 #assume the accuracy of the best value of i is 0
+	best_accSet = c(0,0,0,0,0)
+	
+	#performs 5-fold cross validation using i features with greatest difference in mean
+	for (i in 5:50){ #set to 7 for testing script; change to desired number when actually running
+		acc = c(0,0,0,0,0)
+		for (j in 0:4){
+			xtr = x[-c(1:s+j*s),] #excludes (j+1)th fold for cross validation
+									#do note that we may leave out some samples entirely (up to 4)
+			xtr = data.frame(xtr[,c(22284, m[1:i])]) #glm() wants a dataframe; glm() predicts the first column on the remaining columns
+			xte = x[c(1:s+j*s),] #select the values from the left out fold
+			xte = data.frame(xte[,m[1:i]]) #create a dataframe of the left out fold, for testing
+			yte = x[c(1:s+j*s),22284] #vector of actual classes for the leftout fold, used for comparison to predicted classes
+			model = glm(xtr, family=binomial(link="logit")) #i'm not sure if these are appropriate parameters
+			p= predict(model, xte, type = "response")
+
+			p[which(p>=.5)]=1
+			p[which(p<.5)]=0
+			acc[j+1] = length(which(p==yte))/s
+		}
+		if (mean(acc)>best_acc){
+			best_k = i
+			best_acc = mean(acc)
+			best_accSet = acc
+		}
 	}
-	#3
-	print("----")
-}
 
-#4
-
+	print(best_k)
+	print(best_acc)
+	
 
 
-##work still to do:
+s = floor(n/3) #now split 2/3 train, 1/3 test
+rand = sample(1:n,n) #get a new random permutation
+x = x[rand,]
+xi = x[,c(22284,m[1:best_k])] #use k features according to how many previously determined best 
+xtr = xi[(s+1):n,] #set up dataframe to train on
+xte = xi[1:s,2:(best_k+1)] #set up dataframe to test on; do not include class labels
+yte = xi[1:s,1] #vector of class labels for testing samples
 
-#1will need to convert probabilities to classes, and compare predicted and actual classes for accuracy
-#2record accuracy, and average at the end. 
-#3compare this accuracy to the as of yet best accuracy from using i features, and update best_i and overall_acc as needed
-#4split data into training and test set
-##train glm on train set
-##use to get predicted classes
-##compare to actual classes
+model = glm(xtr, family=binomial(link="logit")) #build
+p = predict(model, xte, type = "response") #predict
+p[which(p>=.5)] = 1 #convert probabilities to labels
+p[which(p<.5)] = 0 #convert probabilities to labels
+print(length(which(p==yte))) #compare predicted and actual labels
+print(length(which(p==yte))/s)
+
+
+
+
+#1456:
+#11121: 86.4%
+#2990: 80.9%
+#2034:
+#4922:
+#7390: 68.2%
